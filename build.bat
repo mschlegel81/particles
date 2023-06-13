34391917f43f37a4b854f9c9c04c466228824256
Martin Schlegel <m.schlegel81@googlemail.com>
2023-06-13 08:45:58 +0200

    Updated build script

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 ..\lazarus64\lazbuild.exe particles.lpi --bm=opt
-..\lazarus64\fpc\3.2.0\bin\x86_64-win64\delp.exe .
-@rem upx.exe --best --ultra-brute -o particles_upx.exe particles.exe
+..\lazarus64\fpc\3.2.2\bin\x86_64-win64\delp.exe -r .
+upx.exe --best --ultra-brute -o particles_upx.exe particles.exe
+move /Y particles_upx.exe particles.exe
